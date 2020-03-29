9944c17f38cf464135628daa3c4bac9fbf9a1055
Ross Nomann <rossnomann@protonmail.com>
2020-03-29 12:48:05 +0400

    Mark utilitly functions as crate-visible (these changes does not affect public API)

--- src/util.rs
+++ src/util.rs
@@ -4,12 +4,12 @@ use std::{
     str,
 };
 
-pub static APE_PREAMBLE: &[u8] = b"APETAGEX";
+pub(super) static APE_PREAMBLE: &[u8] = b"APETAGEX";
 static ID3V1_HEADER: &[u8] = b"TAG";
 static LYRICS3V2_HEADER: &[u8] = b"LYRICS200";
 
 /// Position of ID3v1 tag
-pub const ID3V1_OFFSET: i64 = -128;
+pub(super) const ID3V1_OFFSET: i64 = -128;
 
 /// Number of bytes, which are text digits
 /// that give the total number of bytes
@@ -17,7 +17,7 @@ pub const ID3V1_OFFSET: i64 = -128;
 const LYRICS3V2_SIZE: i64 = 6;
 
 /// Checks whether ape tag exists
-pub fn probe_ape<R: Read + Seek>(reader: &mut R, pos: SeekFrom) -> Result<bool> {
+pub(super) fn probe_ape<R: Read + Seek>(reader: &mut R, pos: SeekFrom) -> Result<bool> {
     let capacity = APE_PREAMBLE.len();
     let mut preamble = Vec::<u8>::with_capacity(capacity);
     reader.seek(pos)?;
@@ -26,7 +26,7 @@ pub fn probe_ape<R: Read + Seek>(reader: &mut R, pos: SeekFrom) -> Result<bool> 
 }
 
 /// Whether ID3v1 tag exists
-pub fn probe_id3v1<R: Read + Seek>(reader: &mut R) -> Result<bool> {
+pub(super) fn probe_id3v1<R: Read + Seek>(reader: &mut R) -> Result<bool> {
     let capacity = ID3V1_HEADER.len();
     let mut header = Vec::<u8>::with_capacity(capacity);
     reader.seek(SeekFrom::End(ID3V1_OFFSET))?;
@@ -36,7 +36,7 @@ pub fn probe_id3v1<R: Read + Seek>(reader: &mut R) -> Result<bool> {
 
 /// Returns the size of the Lyrics3 v2.00 tag or -1 if the tag does not exists.
 /// See http://id3.org/Lyrics3v2 for more details.
-pub fn probe_lyrics3v2<R: Read + Seek>(reader: &mut R) -> Result<i64> {
+pub(super) fn probe_lyrics3v2<R: Read + Seek>(reader: &mut R) -> Result<i64> {
     let capacity = LYRICS3V2_HEADER.len();
     let mut header = Vec::<u8>::with_capacity(capacity);
     reader.seek(SeekFrom::End(ID3V1_OFFSET - capacity as i64))?;
